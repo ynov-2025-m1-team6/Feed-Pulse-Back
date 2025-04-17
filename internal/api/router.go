8da9da4881ed8e0ea9ca262ad080e5a28da1210e
axel senecal <senecal.axel89@gmail.com>
2025-04-17 15:20:11 +0200

    #2 rebase develop

--- internal/api/router.go
+++ internal/api/router.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/ynov-2025-m1-team6/Feed-Pulse-Back/internal/api/handlers"
+	"github.com/ynov-2025-m1-team6/Feed-Pulse-Back/internal/api/handlers/Feedback"
 	"github.com/ynov-2025-m1-team6/Feed-Pulse-Back/internal/api/handlers/auth"
 )
 
@@ -33,4 +34,13 @@ func SetupRoutes(app *fiber.App) {
 	authGrp.Post("/register", auth.RegisterHandler)
 	authGrp.Get("/logout", auth.LogoutHandler)
 
+
+	// Feedback routes
+	feedbacks := app.Group("/feedbacks")
+	feedbacks.Get("/", Feedback.GetAllFeedbacksHandler)
+	feedbacks.Get("/:id", Feedback.GetFeedbackByIDHandler)
+	feedbacks.Post("/", Feedback.CreateFeedbackHandler)
+	feedbacks.Put("/:id", Feedback.UpdateFeedbackHandler)
+	feedbacks.Delete("/:id", Feedback.DeleteFeedbackHandler)
+	feedbacks.Get("/channel/:channel", Feedback.GetFeedbacksByChannelHandler)
 }
