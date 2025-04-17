4e9dc4bb5ec885fd0120a978003e2ada05c35dcd
axel senecal <senecal.axel89@gmail.com>
2025-04-17 15:20:11 +0200

    #2 add feedback orm and crud

--- internal/api/router.go
+++ internal/api/router.go
@@ -5,8 +5,6 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/ynov-2025-m1-team6/Feed-Pulse-Back/internal/api/handlers"
-	"github.com/ynov-2025-m1-team6/Feed-Pulse-Back/internal/api/handlers/Analysis"
-	"github.com/ynov-2025-m1-team6/Feed-Pulse-Back/internal/api/handlers/Feedback"
 	"github.com/ynov-2025-m1-team6/Feed-Pulse-Back/internal/api/handlers/auth"
 )
 
@@ -34,25 +32,4 @@ func SetupRoutes(app *fiber.App) {
 	authGrp.Post("/login", auth.LoginHandler)
 	authGrp.Post("/register", auth.RegisterHandler)
 	authGrp.Get("/logout", auth.LogoutHandler)
-
-
-	// Feedback routes
-	feedbacks := api.Group("/feedbacks")
-	feedbacks.Get("/", Feedback.GetAllFeedbacksHandler)
-	feedbacks.Get("/:id", Feedback.GetFeedbackByIDHandler)
-	feedbacks.Post("/", Feedback.CreateFeedbackHandler)
-	feedbacks.Put("/:id", Feedback.UpdateFeedbackHandler)
-	feedbacks.Delete("/:id", Feedback.DeleteFeedbackHandler)
-	feedbacks.Get("/channel/:channel", Feedback.GetFeedbacksByChannelHandler)
-
-	// Analysis routes
-	analyses := api.Group("/analyses")
-	analyses.Get("/", Analysis.GetAllAnalysesHandler)
-	analyses.Get("/:id", Analysis.GetAnalysisByIDHandler)
-	analyses.Post("/", Analysis.AddAnalysisHandler)
-	analyses.Put("/:id", Analysis.UpdateAnalysisHandler)
-	analyses.Delete("/:id", Analysis.DeleteAnalysisHandler)
-	analyses.Get("/feedback/:feedback_id", Analysis.GetAnalysisByFeedbackIDHandler)
-	analyses.Get("/topic/:topic", Analysis.GetAnalysesByTopicHandler)
-	analyses.Get("/sentiment", Analysis.GetAnalysesBySentimentRangeHandler)
 }
