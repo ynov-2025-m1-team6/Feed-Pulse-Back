10d5940a01280a3862e65c938e424346f76484a0
Luca Morgado <luca.morgado.pro@gmail.com>
2025-04-18 16:28:57 +0200

    #15 added init of mistral client

--- internal/utils/sentimentAnalysis/sentimentAnalysis.go
+++ internal/utils/sentimentAnalysis/sentimentAnalysis.go
@@ -4,20 +4,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gage-technologies/mistral-go"
-	"github.com/tot0p/env"
 	"github.com/ynov-2025-m1-team6/Feed-Pulse-Back/internal/models/Analysis"
 	"github.com/ynov-2025-m1-team6/Feed-Pulse-Back/internal/models/Feedback"
 )
 
+var client *mistral.MistralClient
+
+func InitSentimentAnalysis(apiKey string) {
+	client = mistral.NewMistralClientDefault(apiKey)
+}
+
 func SentimentAnalysis(feedback Feedback.Feedback) (Analysis.Analysis, error) {
-
-	err := env.Load()
-	if err != nil {
-		return Analysis.Analysis{}, fmt.Errorf("failed to load environment variables: %v", err)
-	}
-	// If api key is empty it will load from MISTRAL_API_KEY env var
-	client := mistral.NewMistralClientDefault(env.Get("MISTRAL_API_KEY"))
-
 	// Example: Using Chat Completions
 	chatRes, err := client.Chat("mistral-large-latest", []mistral.ChatMessage{
 		{Content: "I will give you in input sentence, you need to analyse this sentence, give it a score between -1 and +1 (negative,neutral and positive), also give it a topic in french among thoese : 'Support Client,Tarifs et Valeur,Interface Utilisateur,Bugs et Problèmes Techniques,Documentation,Performance,Personnalisation,Processus d’Inscription,Fonctionnalités Avancées,Expérience Utilisateur Générale' and output this in the json format like : \n{\n    \"topic\":\"the theme of the sentence\",\n    \"sentiment_score\":0\n}", Role: mistral.RoleSystem},
