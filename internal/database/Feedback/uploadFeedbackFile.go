2efcc7033bf9a71d1683fd98e009dccbdeef22ba
axel senecal <senecal.axel89@gmail.com>
2025-05-05 12:18:47 +0200

    #14 use CreateFeedback function

--- internal/database/Feedback/uploadFeedbackFile.go
+++ internal/database/Feedback/uploadFeedbackFile.go
@@ -55,9 +55,9 @@ func UploadFeedbacksFromFile(feedbacks []feedbackModel.Feedback) (int, []string,
 		}
 
 		// Create the feedback
-		result := tx.Create(&feedback)
-		if result.Error != nil {
-			errorMsg := fmt.Sprintf("Feedback #%d: %s", i+1, result.Error.Error())
+		_, err := CreateFeedback(feedback)
+		if err != nil {
+			errorMsg := fmt.Sprintf("Feedback #%d: %s", i+1, err.Error())
 			errorMessages = append(errorMessages, errorMsg)
 			continue
 		}
